7537f3c7ae71446300af0c86eb9bec3533f5956c
sakshi <sunshine.chouhan1@gmail.com>
2024-06-12 15:47:31 +0530

    Index file added in project

---
+++ src/main/java/sakshi/group/new_artifact/NewArtifactApplication.java
@@ -0,0 +1,13 @@
+package sakshi.group.new_artifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sakshi/group/new_artifact/NewArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package sakshi.group.new_artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
